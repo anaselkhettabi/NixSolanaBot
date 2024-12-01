cb630f2a13d8c1066290304539759ad41463c35a
anaselkhettabi <104657153+anaselkhettabi@users.noreply.github.com>
2024-11-30 21:26:19 -0500

    Delete nix_bot directory

--- nix_bot/commands/tracking.py
+++
@@ -1,9 +0,0 @@
-from utils.db import get_tracked_wallets
-from telegram import Update
-from telegram.ext import CallbackContext
-
-def track_wallet(update: Update, context: CallbackContext):
-    user_id = update.message.from_user.id
-    wallets = get_tracked_wallets(user_id)
-    wallet_list = "\n".join([f"{w.kol_name}: {w.wallet_address}" for w in wallets])
-    update.message.reply_text(f"Currently tracking:\n{wallet_list}")
--- nix_bot/config.py
+++
@@ -1,2 +0,0 @@
-TELEGRAM_TOKEN = 'YOUR_BOT_TOKEN'
-DATABASE_URI = 'sqlite:///nix_tracker.db'
